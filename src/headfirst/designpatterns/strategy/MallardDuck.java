7b2217a6a63e89f71b5f8846d5835df00015b6c4
Liyanan0410 <35992007+Liyanan0410@users.noreply.github.com>
2019-04-12 16:23:51 +0800

    设计模式 策略模式

---
+++ src/headfirst/designpatterns/strategy/MallardDuck.java
@@ -0,0 +1,13 @@
+package headfirst.designpatterns.strategy;
+
+public class MallardDuck extends Duck {
+
+	public MallardDuck() {
+		flyBehavior = new FlyWithWings();
+		quackBehavior = new Quack();
+	}
+
+	public void display() {
+		System.out.println("I'm a real Mallard duck");
+	}
+}
